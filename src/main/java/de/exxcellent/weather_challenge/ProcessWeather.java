e3935ffef64526770d03c80c54ee0f437165e19b
Azada Henze <rus.azada@gmail.com>
2019-05-06 11:17:50 +0200

    cleaned and documented weather processing  class

--- src/main/java/de/exxcellent/weather_challenge/ProcessWeather.java
+++ src/main/java/de/exxcellent/weather_challenge/ProcessWeather.java
@@ -3,39 +3,43 @@ import java.util.List;
 
 /**
  * ProcessWeather take in weather data -> creates Day Objects -> Stores in Array -> Iterates over the array 
- * -> finds temperature differences -> outputs the day id with the least temperature difference.
+ * -> Finds temperature differences -> Outputs the day id with the least temperature difference.
  * @author Azada Henze
  */
 
 public class ProcessWeather
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    // Declare class variables
     private int tempDiff;
     private int minTempDiff;
     private String dayWithSmallestTempSpread;
     
     public String ProcessWeather() {
-        // Instanzvariable initialisieren
+        // Initialize variables
         tempDiff = 0;
         minTempDiff = 0;
         dayWithSmallestTempSpread = "";
+        //initializea a list to hold day objects
         List<Day> dayList = new ArrayList<Day>();
+        //get the data from the datareader class
         DataReader data = new DataReader();
         List<String[]> weatherDetails = data.readData("input/weather.csv"); // get data from data reader
-        System.out.println("This is dayList" + dayList);   
-        //create Day objects and store in a list
+        //create Day objects and store in THE LIST
         for ( String []  weatherDetail : weatherDetails) {
             Day day = new Day(weatherDetail[0],
             Integer.parseInt(weatherDetail[1]), Integer.parseInt(weatherDetail[2]));
             dayList.add(day);
         }
+        //for each day object calculate the temperature difference and update the minimum temperature difference if its bigger than the current temeperature difference
         for (Day d : dayList) {
             tempDiff = d.getMxT() - d.getMnT();
+            //assign the virdt temperature difference
             if (minTempDiff == 0) {
                 minTempDiff = tempDiff;
            }
            if (tempDiff < minTempDiff){
                minTempDiff = tempDiff;
+               //get the day number of the least temperature spread
                dayWithSmallestTempSpread = d.getDayId();
            }
         }
