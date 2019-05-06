49b0b465d77f40aca3631bd9ee9284d05ba06708
Azada Henze <rus.azada@gmail.com>
2019-05-06 08:23:57 +0200

    create a new project

---
+++ src/main/java/de/exxcellent/weather_challenge/App.java
@@ -0,0 +1,21 @@
+
+/**
+ * This is the main application class of the weather-challenge program. 
+ * 
+ * @author Azada Henze
+ */
+public class App {
+    
+    /**
+     *  This is the main entry method of the weather-challenge program.
+     */
+    
+    public static void main(String args[]) {
+        
+        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+
+        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+    }
+}
