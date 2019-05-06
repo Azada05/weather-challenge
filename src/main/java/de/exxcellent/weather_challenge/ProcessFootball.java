d94bcfe0c222f7dae6be8286f9d17b38bc357970
Azada Henze <rus.azada@gmail.com>
2019-05-06 11:31:52 +0200

    cleaned and documented football processing class

--- src/main/java/de/exxcellent/weather_challenge/ProcessFootball.java
+++ src/main/java/de/exxcellent/weather_challenge/ProcessFootball.java
@@ -9,32 +9,37 @@ import java.lang.Math;
  */
 
 public class ProcessFootball {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    // IDeclare the properties of processing football class
     private int goalDiff;
     private int minGoalDiff;
     private String teamWithSmallestGoalSpread;
     
     public String ProcessFootball() {
-        // Instanzvariable initialisieren
+        // initialize the 
         goalDiff = 0;
         minGoalDiff = 0;
         teamWithSmallestGoalSpread = "";
+        //create a list to store football objects
         List<Team> teamList = new ArrayList<Team>();
+        // get data from data reader
         DataReader data = new DataReader();
-        List<String[]> footballDetails = data.readData("input/football.csv"); // get data from data reader
-        //create Day objects and store in a list
+        List<String[]> footballDetails = data.readData("input/football.csv"); 
+        //create team objects and store in a list
         for ( String []  footballDetail : footballDetails) {
             Team team = new Team(footballDetail[0],
             Integer.parseInt(footballDetail[5]), Integer.parseInt(footballDetail[6]));
             teamList.add(team);
         }
+        //for each team object calculate the goal difference and update the minimum goal difference if its bigger than the current goal difference
         for (Team t : teamList) {
+            //calculate the absolute goal difference
             goalDiff = Math.abs(t.getGoals() - t.getGoalsAllowed());
             if (minGoalDiff == 0) {
                 minGoalDiff = goalDiff;
            }
            if (goalDiff < minGoalDiff){
                minGoalDiff = goalDiff;
+               //get the name of the team with the least range in goal difference
                teamWithSmallestGoalSpread = t.getTeamName();
            }
         }
