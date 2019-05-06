121981d93b42330389883e4fa30ba4e2279f1866
Azada Henze <rus.azada@gmail.com>
2019-05-06 11:12:54 +0200

    cleaned and documented data reading class

--- src/main/java/de/exxcellent/weather_challenge/DataReader.java
+++ src/main/java/de/exxcellent/weather_challenge/DataReader.java
@@ -1,42 +1,37 @@
 import java.io.FileReader;
-import java.util.Arrays;
 import java.io.BufferedReader;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
 
-public class DataReader 
-{
+public class DataReader {
     private static final String DELIMITER = ",";
-    private static List<String []> dataList = new ArrayList<String[]>();
-    public static List<String[]> readData(String file)
-    {
+    private static List<String []> dataList = new ArrayList<String[]>(); //define an Arraylist where each line will be stored as an array of strings
+    public static List<String[]> readData(String file) {
         BufferedReader br = null;
         try{
             //read csv file
             br = new BufferedReader(new FileReader(file));
-            String line="";
+            String line = "";
             //skip the header
-            br.readLine();
+            br.readLine(); 
+            //read from the second line
             while ((line = br.readLine()) != null){
                   String dataDetails []= line.split(DELIMITER);
                   if (dataDetails.length > 0) {
                         dataList.add(dataDetails);
-                    }
+                  }
             }
         }
-        catch (Exception ee)
-        {
+        //handle exceptions
+        catch (Exception ee){
         ee.printStackTrace();
         }
-        finally 
-        {
-            try
-            {
+        finally {
+            try {
                 br.close();
             }
-            catch(IOException ie) 
-            {
+            catch(IOException ie) {
                 System.out.println("Error parsing the file");
                 ie.printStackTrace();
             }
